f6a53799dba9a7942d52801f99a2b973ae7697a7
guiguan <guiguanqiang@163.com>
2018-09-01 17:28:54 +0800

    first commit

---
+++ src/main/java/com/imooc/mmall/MmallCloudApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.mmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmallCloudApplication.class, args);
+    }
+}
