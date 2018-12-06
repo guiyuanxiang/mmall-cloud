b547e388db818c4b82d21ed51b9262e02ff72436
guiguan <guiguanqiang@163.com>
2018-12-06 21:15:18 +0800

    product逻辑开始构建，下一步sql优化

--- mmall-product/src/main/java/com/mmall/mmallproduct/MmallProductApplication.java
+++ mmall-product/src/main/java/com/mmall/mmallproduct/MmallProductApplication.java
@@ -2,11 +2,14 @@ package com.mmall.mmallproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MmallProductApplication {
 
     public static void main(String[] args) {
+        //todo sql优化
         SpringApplication.run(MmallProductApplication.class, args);
     }
 }
