0bc35b4aa8d5b05ba9869d59e8a8afff88d32dfc
guiguan <guiguanqiang@163.com>
2018-12-11 17:18:32 +0800

    druid 配置修改

--- mmall-product/src/main/java/com/mmall/mmallproduct/pojo/vo/ItemVo.java
+++ mmall-product/src/main/java/com/mmall/mmallproduct/pojo/vo/ItemVo.java
@@ -1,14 +1,104 @@
 package com.mmall.mmallproduct.pojo.vo;
 
-import com.mmall.mmallproduct.pojo.domain.Item;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author: gg
  * @date: 2018/12/10 17:35
  */
 @Data
-public class ItemVo extends Item implements Serializable{
+public class ItemVo implements Serializable{
+
+    /**
+     * 商品id
+     */
+    private Integer id;
+
+    /**
+     * 商品名称
+     */
+    private String title;
+
+    /**
+     * 商品编码
+     */
+    private String itemCode;
+
+    /**
+     * 商品条码
+     */
+    private String barCode;
+
+    /**
+     * 商品属性
+     */
+    private String property;
+
+    /**
+     * 商品卖点
+     */
+    private String sellingPoint;
+
+    /**
+     * 购买提醒
+     */
+    private String buyRemind;
+
+    /**
+     * 保质期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
+    private Date expirationDate;
+
+
+    /**
+     * 商品标签
+     */
+    private String tags;
+
+
+    /**
+     * 商品类目id
+     */
+    private Integer categoryId;
+
+    /**
+     * 商品品牌id
+     */
+    private Integer brandId;
+
+    /**
+     * 单位id
+     */
+    private Integer unitId;
+
+    /**
+     * 是否有保质期 0-无 1-有
+     */
+    private Integer expirationDateStatus;
+
+    /**
+     * 商品类型 0-实物 1-虚拟类商品比如服务
+     */
+    private Integer itemType;
+
+    /**
+     * 是否是赠品 0-不是 1-是
+     */
+    private Boolean isPresent;
+
+    /**
+     * 是否放入回收站 0-不是 1-是
+     */
+    private Boolean isRecycle;
+
+    /**
+     * 1-删除 0-正常
+     */
+    private Boolean isDeleted;
+
 }
